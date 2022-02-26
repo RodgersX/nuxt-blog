b7288733eca0ef5b69a12d07cfd3e159785c8554
RodgersX <brianmawira2@gmail.com>
2022-02-26 12:30:11 +0300

    COMMIT: added navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSideNav :show="displaySidenav" @close="displaySidenav = false" />
     <Nuxt />
   </div>
 </template>
 
+<script>
+export default {
+  components: {
+    TheHeader: require('~/components/Navigation/TheHeader.vue').default,
+    TheSideNav: require('~/components/Navigation/TheSidenav.vue').default,
+  },
+  data() {
+    return {
+      displaySidenav: false,
+    }
+  },
+}
+</script>
 <style>
 html {
   font-family: 'Open Sans', sans-serif;
