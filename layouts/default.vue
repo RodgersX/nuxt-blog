11773f50ff050d8f5be918b9efd2e8848abef3d5
RodgersX <brianmawira2@gmail.com>
2022-02-26 12:19:31 +0300

    adding static images

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,13 @@
     <Nuxt />
   </div>
 </template>
+
+<style>
+html {
+  font-family: 'Open Sans', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,77 @@
 <template>
-
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts">
+      <PostPreview
+        id="1"
+        thumbnail="https://institute.global/sites/default/files/styles/teaser_large/public/2021-06/GettyImages-1165900389.jpg?itok=f-DcPSZ9"
+        title="Hello there!"
+        preview-text="This is my first post!"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="https://institute.global/sites/default/files/styles/teaser_large/public/2021-06/GettyImages-1165900389.jpg?itok=f-DcPSZ9"
+        title="Hello there - the second time!"
+        preview-text="This is my second post!"
+      />
+      <PostPreview
+        id="3"
+        thumbnail="https://institute.global/sites/default/files/styles/teaser_large/public/2021-06/GettyImages-1165900389.jpg?itok=f-DcPSZ9"
+        title="Hello!"
+        preview-text="This is my third post!"
+      />
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  components: {
+    PostPreview: require('~/components/Posts/PostPreview.vue').default,
+  },
 }
 </script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-image: url('~assets/images/main-page-background.jpg');
+  background-position: center;
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
